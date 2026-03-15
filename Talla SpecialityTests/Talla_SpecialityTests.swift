50b09d1d5460b92086aa62d0ec71c7d8a4cfef76
Ahmad AlBuainain <Ahmad.Alweswasi@gmail.com>
2026-03-15 17:52:09 +0300

    Initial Commit

---
+++ Talla SpecialityTests/Talla_SpecialityTests.swift
@@ -0,0 +1,16 @@
+//
+//  Talla_SpecialityTests.swift
+//  Talla SpecialityTests
+//
+//  Created by Ahmad AlBuainain on 15/3/26.
+//
+
+import Testing
+
+struct Talla_SpecialityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Talla SpecialityUITests/Talla_SpecialityUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Talla_SpecialityUITestsLaunchTests.swift
+//  Talla SpecialityUITests
+//
+//  Created by Ahmad AlBuainain on 15/3/26.
+//
+
+import XCTest
+
+final class Talla_SpecialityUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
